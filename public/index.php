5458b787ee2feb31048cbf5cef299d9bc2dbc0df
ktomaszewski <tomaszewski.kamil.97@gmail.com>
2021-09-16 22:11:10 +0200

    Refactor

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
-// Delegate static file requests back to the PHP built-in webserver
+use Mezzio\Application;
+use Mezzio\MiddlewareFactory;
+use Psr\Container\ContainerInterface;
+
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
@@ -10,21 +13,16 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-/**
- * Self-called anonymous function that creates its own scope and keeps the global namespace clean.
- */
-(function () {
-    /** @var \Psr\Container\ContainerInterface $container */
+(static function () {
+    /** @var ContainerInterface $container */
     $container = require 'config/container.php';
 
-    /** @var \Mezzio\Application $app */
-    $app = $container->get(\Mezzio\Application::class);
-    $factory = $container->get(\Mezzio\MiddlewareFactory::class);
+    /** @var Application $application */
+    $application = $container->get(Application::class);
+    $middlewareFactory = $container->get(MiddlewareFactory::class);
 
-    // Execute programmatic/declarative middleware pipeline and routing
-    // configuration statements
-    (require 'config/pipeline.php')($app, $factory, $container);
-    (require 'config/routes.php')($app, $factory, $container);
+    (require 'config/pipeline.php')($application, $middlewareFactory, $container);
+    (require 'config/routes.php')($application, $middlewareFactory, $container);
 
-    $app->run();
+    $application->run();
 })();
